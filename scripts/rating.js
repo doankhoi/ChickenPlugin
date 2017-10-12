9af0a33ac643e5f7a90bf96a99b6fa5c8c8ce2e1
thuync <nguyenchungthuy.hust@gmail.com>
2017-10-12 22:46:56 +0700

    Parse reviews

--- scripts/rating.js
+++ scripts/rating.js
@@ -3,6 +3,47 @@ var XPATH = {
 	'percent_stars': "//main[@id='site-content']/div[@id='host-dashboard-container']//div[@class='row'][1]/div[@class='col-lg-8']//table[@class='_zdxht7']//tr[@class='_mcc0b8']/td[@class='_18j2f1c']/div[@class='_rotqmn2']",
 };
 
-$(function() {
-	console.log($(document).xpath(XPATH['total_stats']));
-});
+
+function parseReview(element) {
+    var publicFeedbackElement = $(element).xpath(".//span[@class='_16yk9f2m']");
+
+    var detailRatingElements = $(element).xpath(".//div[@class='_1rlifxji']");
+
+    $(detailRatingElements).children().each(function () {
+        parseDetailRating($(this));
+    })
+}
+
+function parseDetailRating(element) {
+    var criteriaElement = $(element).xpath(".//div[@class='_33hj8bi']/span");
+    var ratingElement = $(element).xpath(".//div[@class='_y4f73f']/span");
+
+
+    if (!criteriaElement.length && !ratingElement.length) {
+        console.log(criteriaElement, ratingElement);
+        var criteria = $(criteriaElement[0]).text();
+        var rating = parseDetailRating($(ratingElement[0]).attr('aria-label'));
+        console.log('CRITERIA', criteria);
+        console.log('RATING', rating);
+    }
+}
+
+function parseRatingValue(ratingLabel) {
+    console.log('RATING LABEL', ratingLabel);
+    // Rating 5 out of 5
+    var result = ratingLabel.match(/(\d) out of/);
+    if (result) {
+        return parseInt(result[1]);
+    }
+
+    return -1;
+}
+
+function parseReviews() {
+    var reviewElements = $(document).xpath("//div[@class='_1f9rmq80']");
+    reviewElements.each(function () {
+        parseReview($(this));
+    });
+}
+
+parseReviews();
