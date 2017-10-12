f626610a2074c77019e1b1f79abc90ea1ef7a247
doankhoi <doanngockhoi93@gmail.com>
2017-10-12 15:35:31 +0700

    Add lib xpath

--- scripts/rating.js
+++ scripts/rating.js
@@ -1,15 +1,4 @@
 var XPATH = {
-	'overall_rating': '',
-	'total_reviews': '',
-	'total_percent_5_reviews': '',
-	'percent_5_star': '',
-	'percent_4_star': '',
-	'percent_3_star': '',
-	'percent_2_star': '',
-	'percent_1_star': '',
-	'list_reviews': [
-
-	]
+	'total_stats': "//main[@id='site-content']/div[@id='host-dashboard-container']//div[@class='row']/div[@class='col-lg-8']//div/div[@class='_14bk9xpe']",
+	'percent_stars': "//main[@id='site-content']/div[@id='host-dashboard-container']//div[@class='row'][1]/div[@class='col-lg-8']//table[@class='_zdxht7']//tr[@class='_mcc0b8']/td[@class='_18j2f1c']/div[@class='_rotqmn2']",
 };
-
-console.log("OK that good");
