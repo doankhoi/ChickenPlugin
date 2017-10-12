e49dcfc3e9f37c85fb6351c09bdfadabc8e0610d
doankhoi <doanngockhoi93@gmail.com>
2017-10-12 22:49:29 +0700

    Refactor

--- scripts/rating.js
+++ scripts/rating.js
@@ -1,25 +1,8 @@
 var XPATH = {
   'total_stats': "//div[@class='_14bk9xpe']",
   'percent_stars': "//td[@class='_18j2f1c']/div[@class='_rotqmn2']",
+  'next_page_review': "//button[@class='_1rp5252']"
 };
-
-var eventMatchers = {
-    'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
-    'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
-}
-
-var defaultOptions = {
-    pointerX: 0,
-    pointerY: 0,
-    button: 0,
-    ctrlKey: false,
-    altKey: false,
-    shiftKey: false,
-    metaKey: false,
-    bubbles: true,
-    cancelable: true
-}
-
 
 function parseTotalRating() {
   var raw_total = $(document).xpath(XPATH['total_stats']);
@@ -53,49 +36,8 @@ function parsePercentStars() {
   return results;
 }
 
-
-function simulate(element, eventName) {
-    var options = extend(defaultOptions, arguments[2] || {});
-    var oEvent, eventType = null;
-
-    for (var name in eventMatchers) {
-        if (eventMatchers[name].test(eventName)) { 
-          eventType = name;
-          break; 
-        }
-    }
-
-    if (!eventType)
-      throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
-
-    if (document.createEvent) {
-        oEvent = document.createEvent(eventType);
-        if (eventType == 'HTMLEvents') {
-            oEvent.initEvent(eventName, options.bubbles, options.cancelable);
-        } else {
-            oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
-            options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
-            options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
-        }
-        element.dispatchEvent(oEvent);
-    } else {
-        options.clientX = options.pointerX;
-        options.clientY = options.pointerY;
-        var evt = document.createEventObject();
-        oEvent = extend(evt, options);
-        element.fireEvent('on' + eventName, oEvent);
-    }
-    return element;
-}
-
-function extend(destination, source) {
-    for (var property in source)
-      destination[property] = source[property];
-    return destination;
-}
-
 setTimeout(function() {
-  var erns = $(document).xpath("//div[@class='_1xal37k']/a[@class='_13g6i3a1'][1]")[0];
-  simulate(erns, "click");
+  var next_button = $(document).xpath(XPATH['next_page_review'])[1];
+  simulateMouse(next_button, "click");
 }, 3000);
 
