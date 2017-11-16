eda6c1d2be9007ea203d11254070550f26278dc4
doankhoi <doanngockhoi93@gmail.com>
2017-11-16 16:20:18 +0700

    Add css

--- make_dist.sh
+++ make_dist.sh
@@ -1,5 +1,5 @@
 mkdir -p dist
 rm -rf dist/*
-cp -r background.js content.js popup.html images lib manifest.json scripts utils dist
+cp -r background.js content.js popup.html images lib manifest.json scripts utils css dist
 rm cohost.zip
 zip -r cohost.zip dist/
